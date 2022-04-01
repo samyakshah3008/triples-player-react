852c84c0a858d0c729f27d0549da0eef08af3fde
Samyak Shah <samyakshah3008@gmail.com>
2022-04-02 01:40:20 +0530

    FEAT: Landingpage component add

--- src/pages/landingpage/LandingPage.jsx
+++ src/pages/landingpage/LandingPage.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import HeroHeader from '../../components/navbar/hero-header/HeroHeader'
-import Navbar from '../../components/navbar/Navbar'
-import "./landingpage.css"
+import React from "react";
+import HeroHeader from "../../components/hero-header/HeroHeader";
+import Navbar from "../../components/navbar/Navbar";
+import "./landingpage.css";
+import nikeShoes from "../../assets/cricket-ball.jpg";
 
 export default function LandingPage() {
   return (
     <div>
-         <div className="home-container">
-            <img
-                src="https://res.cloudinary.com/dhqxln7zi/image/upload/v1648118531/new_vohp8t.jpg"
-                alt="landing"
-                className="responsive-images landing-image"
-            />
-            <div className="landing-container">
-            <Navbar/>
-        <HeroHeader/>
-            </div>
+      <div className="home-container">
+        <img
+          src={nikeShoes}
+          alt="landing"
+          className="responsive-images landing-image"
+        />
+        <div className="landing-container">
+          <Navbar />
+          <HeroHeader />
         </div>
-        
+      </div>
     </div>
-  )
+  );
 }
