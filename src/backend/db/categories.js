e0cca1fd3358e909f0b1122a8bdd52a8d1742770
Samyak Shah <samyakshah3008@gmail.com>
2022-04-02 01:33:09 +0530

    Database updated

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,5 +1,5 @@
 import { v4 as uuid } from "uuid";
-
+import cricketBall from "../../assets/cricket-ball.jpg"
 /**
  * Category Database can be added here.
  * You can add category of your wish with different attributes
@@ -9,19 +9,56 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Computer Programming",
+    cardImg: cricketBall,
+    cardTitle: "Cricket ball",
+    cardOwner: "Samyak Shah",
     description:
       "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
+    categoryName: "Computer Programming",
+    cardImg: cricketBall,
+    cardTitle: "Cricket ball",
+    cardOwner: "Samyak Shah",
     description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
+    categoryName: "Computer Programming",
+    cardImg: cricketBall,
+    cardTitle: "Cricket ball",
+    cardOwner: "Samyak Shah",
     description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
   },
+  {
+    _id: uuid(),
+    categoryName: "Computer Programming",
+    cardImg: cricketBall,
+    cardTitle: "Cricket ball",
+    cardOwner: "Samyak Shah",
+    description:
+      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Computer Programming",
+    cardImg: cricketBall,
+    cardTitle: "Cricket ball",
+    cardOwner: "Samyak Shah",
+    description:
+      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Computer Programming",
+    cardImg: cricketBall,
+    cardTitle: "Cricket ball",
+    cardOwner: "Samyak Shah",
+    description:
+      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+  },
+  
 ];
