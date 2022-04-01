89a0ba196550488840ce9b5366fe59f055b4ed0a
Samyak Shah <samyakshah3008@gmail.com>
2022-04-02 01:41:11 +0530

    FEAT: Signuppage component add

---
+++ src/pages/signuppage/Signuppage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Signup from '../../components/signup/Signup'
+
+export default function Signuppage() {
+  return (
+    <div>
+        <Navbar/>
+        <Signup/>
+    </div>
+  )
+}
