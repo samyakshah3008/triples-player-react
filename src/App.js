2c27a70bad00ccc9d6379b4103d2c60617c4edf8
Samyak Shah <samyakshah3008@gmail.com>
2022-04-02 01:30:42 +0530

    Routes add for different pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import "./App.css";
 import LandingPage from "./pages/landingpage/LandingPage";
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import VideoListingPage from "./pages/video-listing-page/VideoListingPage";
+import LikePage from "./pages/likepage/LikePage";
+import HistoryPage from "./pages/historypage/HistoryPage";
+import PlaylistPage from "./pages/playlist/PlaylistPage";
+import Watchlaterpage from "./pages/watchlaterpage/Watchlaterpage";
+import LoginPage from "./pages/loginpage/LoginPage";
+import Signuppage from "./pages/signuppage/Signuppage";
 function App() {
-
   return (
     <div className="App">
-    <Routes>
-      <Route path="/" element = {<LandingPage/>}></Route>
-      <Route path="/navbar" element = {<Navbar/>}></Route>
-
-
-    </Routes>
-
-
-         </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/videolisting" element={<VideoListingPage />}></Route>
+        <Route path="/like" element={<LikePage />}></Route>
+        <Route path="/history" element={<HistoryPage />}></Route>
+        <Route path="/playlist" element={<PlaylistPage />}></Route>
+        <Route path="/watchlater" element={<Watchlaterpage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<Signuppage />}></Route>
+      </Routes>
+    </div>
   );
 }
 
