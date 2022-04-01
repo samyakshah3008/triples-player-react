4d492a24fb627131f26a694536d5e87008119bc3
Samyak Shah <samyakshah3008@gmail.com>
2022-04-02 01:39:56 +0530

    FEAT: Watchlaterbox component add

---
+++ src/components/watchlaterbox/WatchLaterBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function WatchLaterBox() {
+  return (
+    <div>
+     <div className="video-listing-container">
+      <div className="page-title">Watch later video (0) </div>
+      <div className="page-video-container">
+          <div className="no-video-message">There Is No watch later Video. Please Add </div>
+          <button className="explore-btn">Explore</button>
+          
+      </div>
+    </div>
+    </div>
+  )
+}
