ece4ba708b461cad3ab41f02f8b325dd970e6a7d
Samyak Shah <samyakshah3008@gmail.com>
2022-04-03 00:06:41 +0530

    login component add

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./login.css";
+import { useState } from "react";
+import { useAuth } from "../../contexts/auth-context";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [userDetail, setUserDetail] = useState({ email: "", password: "" });
+
+  const { setUser } = useAuth();
+
+  const changeHandler = (e) => {
+    setUserDetail((previousState) => ({
+      ...previousState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const navigate = useNavigate();
+
+  const loginHandler = async (isUser) => {
+    try {
+      const loginData = await axios.post(
+        "/api/auth/login",
+        isUser
+          ? { email: "adarshbalika@gmail.com", password: "adarshBalika123" }
+          : userDetail
+      );
+      setUser({
+        user: loginData.data.foundUser,
+        token: loginData.data.encodedToken,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   return (
     <div>
       <div className="login-container">
@@ -11,6 +46,9 @@ export default function Login() {
           <div className="input-container">
             <div className="input-title">Email:</div>
             <input
+              name="email"
+              onChange={(e) => changeHandler(e)}
+              value={userDetail.email}
               type="email"
               placeholder="Enter email"
               className="input-field"
@@ -19,13 +57,24 @@ export default function Login() {
           <div className="input-container">
             <div className="input-title">Password:</div>
             <input
+              name="password"
+              onChange={(e) => changeHandler(e)}
+              value={userDetail.password}
               type="password"
               placeholder="Enter password"
               className="input-field"
             />
           </div>
-          <button className="input-login-btn">Login</button>
-          <div className="test-credential-login-link">
+          <button
+            onClick={() => loginHandler(false)}
+            className="input-login-btn"
+          >
+            Login
+          </button>
+          <div
+            onClick={() => loginHandler(true)}
+            className="test-credential-login-link"
+          >
             Login with test credential
           </div>
           <Link to="/signup">
