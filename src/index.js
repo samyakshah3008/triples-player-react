23678c087a7d843437e04a9fe3ebff26561c1032
Samyak Shah <samyakshah3008@gmail.com>
2022-04-02 16:37:57 +0530

    context for video listing add in tree

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import { VideoProvider } from "./contexts/video-listing-context";
 
 // Call make Server
 makeServer();
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <VideoProvider>
     <App />
+    </VideoProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
