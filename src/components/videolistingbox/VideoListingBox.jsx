d5ab157d47cd82dff4d9b9e56d4a8d89b6d92596
Samyak Shah <samyakshah3008@gmail.com>
2022-04-02 16:39:32 +0530

    videolistingbox component update

--- src/components/videolistingbox/VideoListingBox.jsx
+++ src/components/videolistingbox/VideoListingBox.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useVideoList } from "../../contexts/video-listing-context";
 import Card from "../card/Card";
 import "./video-listing-box.css";
 
-export default function VideoListingBox() {
+export default function VideoListingBox() { 
+
+  const {videoList, setVideoList} = useVideoList()
+console.log(videoList)
   return (
     <div>
       <div className="video-listing-container">
-        <Card />
+        {videoList.map((item) => (
+        <Card item = {item} />
+        ))}
       </div>
+    
+
     </div>
   );
 }
