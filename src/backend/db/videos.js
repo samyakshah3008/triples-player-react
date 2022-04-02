32d1683b833111193c4731aed8d86a869a043c13
Samyak Shah <samyakshah3008@gmail.com>
2022-04-02 16:38:07 +0530

    database updated

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -1,3 +1,6 @@
+import { v4 as uuid } from "uuid";
+
+
 /**
  * Videos Database can be added here.
  * You can add videos of your wish with different attributes
@@ -5,17 +8,220 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: uuid(),
+    categoryName: "Comedy",
+    cardImg: "https://img.youtube.com/vi/LKNHVDPKy7g/hqdefault.jpg",
+    cardTitle: "Dhamaal",
+    cardOwner: "Samyak Shah",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: uuid(),
+    categoryName: "Comedy",
+    cardImg: "https://img.youtube.com/vi/SodVhwB6Sz8/hqdefault.jpg",
+    cardTitle: "Golmaal",
+    cardOwner: "Samyak Shah",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
   },
+  {
+    _id: uuid(),
+    categoryName: "Comedy",
+    cardImg: "https://img.youtube.com/vi/iGfT6rdTS34/hqdefault.jpg",
+    cardTitle: "Welcome",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Comedy",
+    cardImg: "https://img.youtube.com/vi/LXXkiUKDK4w/hqdefault.jpg",
+    cardTitle: "Entertainment",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Comedy",
+    cardImg: "https://img.youtube.com/vi/tPXYWRGNl7s/hqdefault.jpg",
+    cardTitle: "Ready",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Comedy",
+    cardImg: "https://img.youtube.com/vi/SGwFnnS0iu4/hqdefault.jpg",
+    cardTitle: "Housefull 2",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Horror",
+    cardImg: "https://img.youtube.com/vi/yftMYuacQio/hqdefault.jpg",
+    cardTitle: "The Conjuring",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Horror",
+    cardImg: "https://img.youtube.com/vi/eAJFVeBPXjQ/hqdefault.jpg",
+    cardTitle: "Annabelle comes home",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Horror",
+    cardImg: "https://img.youtube.com/vi/VSaEdshUCzA/hqdefault.jpg",
+    cardTitle: "The Nun",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Horror",
+    cardImg: "https://img.youtube.com/vi/yrgst6eD-Dk/hqdefault.jpg",
+    cardTitle: "Conjuring summary",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Horror",
+    cardImg: "https://img.youtube.com/vi/3gJId-oxtpc/hqdefault.jpg",
+    cardTitle: "Haunted House",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Horror",
+    cardImg: "https://img.youtube.com/vi/quNYPP97Yes/hqdefault.jpg",
+    cardTitle: "The 1920 Evil Returns",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Thrill and action",
+    cardImg: "https://img.youtube.com/vi/S8ZUtWTuHT4/hqdefault.jpg",
+    cardTitle: "Spiderman No way home",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Thrill and action",
+    cardImg: "https://img.youtube.com/vi/DgfsnXSOeZk/hqdefault.jpg",
+    cardTitle: "Krish 3",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Thrill and action",
+    cardImg: "https://img.youtube.com/vi/-8p1H4A5jvQ/hqdefault.jpg",
+    cardTitle: "Ghajni",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Thrill and action",
+    cardImg: "https://img.youtube.com/vi/zBXa6Foq9ZM/hqdefault.jpg",
+    cardTitle: "Bang Bang!",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Thrill and action",
+    cardImg: "https://img.youtube.com/vi/Jr3yUz0jRpg/hqdefault.jpg",
+    cardTitle: "Race 2",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Thrill and action",
+    cardImg: "https://img.youtube.com/vi/n8iqixi7lj8/hqdefault.jpg",
+    cardTitle: "Dhoom 3",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Documentary",
+    cardImg: "https://img.youtube.com/vi/E00892hmVZM/hqdefault.jpg",
+    cardTitle: "MS Dhoni: Untold Story",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Documentary",
+    cardImg: "https://img.youtube.com/vi/RPJP9fFnMoU/hqdefault.jpg",
+    cardTitle: "83 Full Movie",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Documentary",
+    cardImg: "https://img.youtube.com/vi/xHfdt5cnrZ8/hqdefault.jpg",
+    cardTitle: "Mumbai central",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Documentary",
+    cardImg: "https://img.youtube.com/vi/fO1mbR7zyF8/hqdefault.jpg",
+    cardTitle: "Sanju",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Documentary",
+    cardImg: "https://img.youtube.com/vi/WvS9jmc2-kw/hqdefault.jpg",
+    cardTitle: "The Kashmir files",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Documentary",
+    cardImg: "https://img.youtube.com/vi/LGB61HyL-IQ/hqdefault.jpg",
+    cardTitle: "Chakde India",
+    cardOwner: "Samyak Shah",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+  },
+
 ];
