5016d070cd56e5c9297547f99c043f890df2aaf9
Samyak Shah <samyakshah3008@gmail.com>
2022-04-05 01:41:39 +0530

    card update

---
+++ src/components/card/Card2.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./card.css";
+import * as FaIcons from "react-icons/fa";
+
+
+export default function Card2({item}) {
+  return (
+    <div>
+        <div className="parent">
+        <div className="card-container">
+          <div className="card-img-container">
+            <img className="card-img" src={item.cardImg} alt="cricket-ball" />
+          </div>
+          <div className="card-sub-container">
+            <img src={item.cardImg} className="avatar-img"></img>
+            <div className="card-title"> {item.cardTitle} </div>
+            {/* <FaIcons.FaThumbsUp onClick={likeHandler} className="icons sidebar-icons" /> */}
+            <FaIcons.FaClock className="icons sidebar-icons" />
+            <FaIcons.FaFolderPlus className="icons sidebar-icons" />
+          </div>
+          <div className="card-sub-container2">
+            <div className="card-channel-name"> {item.cardOwner} </div>
+            <FaIcons.FaCheckCircle className="icons sidebar-icons" />
+          </div>
+          <div className="card-sub-container3">
+           {item.description}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
