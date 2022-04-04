68837b059072e4b919410e7f8c7fdeff4d1c4f70
Samyak Shah <samyakshah3008@gmail.com>
2022-04-05 01:41:34 +0530

    card update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,30 +1,140 @@
 import React from "react";
 import "./card.css";
-import cricketBall from "../../assets/cricket-ball.jpg";
 import * as FaIcons from "react-icons/fa";
+import { useLike } from "../../contexts/like-context";
+import axios from "axios";
+import { useAuth } from "../../contexts/auth-context";
+import { useWatchLater } from "../../contexts/watch-later-context";
+import { useEffect } from "react";
 
-export default function Card({item}) {
+export default function Card({ item, isLike, isWatchLater }) {
+  const { like, setLike } = useLike();
+  const { user } = useAuth();
+  const { watchLater, setWatchLater } = useWatchLater();
+
+  useEffect(() => {
+    user.token
+      ? (async () => {
+          try {
+            const responseFromServer = await axios.get("/api/user/watchlater", {
+              headers: { authorization: user.token },
+            });
+
+            if (responseFromServer.status === 200) {
+              setWatchLater({ watchLater: responseFromServer.data.watchlater });
+            }
+          } catch (err) {
+            console.error("error", err);
+          }
+        })()
+      : setWatchLater({ watchLater: [] });
+  }, []);
+
+  const likeHandler = async () => {
+    // console.log(like)
+
+    try {
+      const likeResponse = await axios({
+        method: "post",
+        url: "/api/user/likes",
+        headers: { authorization: user.token },
+        data: { video: item },
+      });
+      setLike({ like: likeResponse.data.likes });
+
+      // console.log(likeResponse.data.likes)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeLikeHandler = async () => {
+    try {
+      const removeLikeResponse = await axios({
+        method: "delete",
+        url: `/api/user/likes/${item._id}`,
+        headers: { authorization: user.token },
+        data: { video: item },
+      });
+
+      setLike({ like: removeLikeResponse.data.likes });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const moveToWatchLaterHandler = async () => {
+    try {
+      const watchLaterData = await axios({
+        method: "post",
+        url: "/api/user/watchlater",
+        headers: { authorization: user.token },
+        data: { video: item },
+      });
+      setWatchLater({ watchLater: watchLaterData.data.watchlater });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeFromWatchLaterHandler = async () => {
+    try {
+      const removeWatchLaterResponse = await axios({
+        method: "delete",
+        url: `/api/user/watchlater/${item._id}`,
+        headers: { authorization: user.token },
+        data: { video: item },
+      });
+
+      setWatchLater({ watchLater: removeWatchLaterResponse.data.watchlater });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div className="parent">
         <div className="card-container">
           <div className="card-img-container">
+            {/* <video controls> <source src= {item.videoLink} type = "video/mp4"/>  </video> */}
             <img className="card-img" src={item.cardImg} alt="cricket-ball" />
           </div>
           <div className="card-sub-container">
             <img src={item.cardImg} className="avatar-img"></img>
             <div className="card-title"> {item.cardTitle} </div>
-            <FaIcons.FaThumbsUp className="icons sidebar-icons" />
-            <FaIcons.FaClock className="icons sidebar-icons" />
+            {/* <FaIcons.FaTrash className="icons" /> */}
+            {isLike ? (
+              <FaIcons.FaThumbsUp
+                onClick={removeLikeHandler}
+                className="icons sidebar-icons"
+              />
+            ) : (
+              <FaIcons.FaRegThumbsUp
+                onClick={likeHandler}
+                className="icon-outline-blue"
+              />
+            )}
+
+            {isWatchLater ? (
+              <FaIcons.FaClock
+                onClick={removeFromWatchLaterHandler}
+                className="icons sidebar-icons"
+              />
+            ) : (
+              <FaIcons.FaRegClock
+                onClick={moveToWatchLaterHandler}
+                className="icon-outline-blue"
+              />
+            )}
+
             <FaIcons.FaFolderPlus className="icons sidebar-icons" />
           </div>
           <div className="card-sub-container2">
             <div className="card-channel-name"> {item.cardOwner} </div>
             <FaIcons.FaCheckCircle className="icons sidebar-icons" />
           </div>
-          <div className="card-sub-container3">
-           {item.description}
-          </div>
+          <div className="card-sub-container3">{item.description}</div>
         </div>
       </div>
     </div>
