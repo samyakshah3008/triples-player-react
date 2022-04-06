948d28f9d481635f0d5748f2d95198bea537c067
Samyak Shah <samyakshah3008@gmail.com>
2022-04-07 00:03:55 +0530

    FEAT: videolist by category update

--- src/components/videolistingbox/VideoListingBox.jsx
+++ src/components/videolistingbox/VideoListingBox.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { useState, useReducer } from "react";
+import { useEffect } from "react";
 import { useLike } from "../../contexts/like-context";
 import { useVideoList } from "../../contexts/video-listing-context";
 import { useWatchLater } from "../../contexts/watch-later-context";
 import Card from "../card/Card";
 import "./video-listing-box.css";
 
-export default function VideoListingBox() {
-  const [active, setActive] = useState("ALL");
+const filteredList = [
+  "ALL",
+  "HORROR",
+  "COMEDY",
+  "THRILL AND ACTION",
+  "DOCUMENTARY",
+];
 
+export default function VideoListingBox() {
+  const [filter, setFilter] = useState("ALL");
+  const [filteredData, setFilteredData] = useState([]);
   const { videoList } = useVideoList();
 
   const { like } = useLike();
@@ -16,67 +25,34 @@ export default function VideoListingBox() {
   const { watchLater } = useWatchLater();
 
   const watchLaterId = watchLater.watchLater.map((item) => item._id);
-  // console.log(watchLaterId)
   const likeId = like.like.map((item) => item._id);
-  // console.log(likeId)
 
-  const AllClickHandler = () => {
-    setActive("ALL");
-  };
-
-  const comedyClickHandler = () => {
-    setActive("COMEDY");
-  };
-  const horrorClickHandler = () => {
-    setActive("HORROR");
-  };
-  const thrillClickHandler = () => {
-    setActive("THRILL");
-  };
-
-  const documentaryClickHandler = () => {
-    setActive("DOCUMENTARY");
-  };
-
-  // console.log(videoList)
-
+  useEffect(() => {
+    setFilteredData(
+      videoList.filter((data) =>
+        filter === "ALL"
+          ? true
+          : data.categoryName.toLowerCase() === filter.toLowerCase()
+      )
+    );
+  }, [filter]);
   return (
     <div>
       <div className="chips-container">
-        <button
-          onClick={AllClickHandler}
-          style={{ backgroundColor: active }}
-          className={`${active === "ALL" && "chip-active"} chips`}
-        >
-          All
-        </button>
-        <button
-          onClick={comedyClickHandler}
-          className={`${active === "COMEDY" && "chip-active"} chips`}
-        >
-          Comedy
-        </button>
-        <button
-          onClick={horrorClickHandler}
-          className={`${active === "HORROR" && "chip-active"} chips`}
-        >
-          Horror
-        </button>
-        <button
-          onClick={thrillClickHandler}
-          className={`${active === "THRILL" && "chip-active"} chips`}
-        >
-          Thrill and action
-        </button>
-        <button
-          onClick={documentaryClickHandler}
-          className={`${active === "DOCUMENTARY" && "chip-active"} chips`}
-        >
-          Documentary
-        </button>
+        {filteredList.map((element) => {
+          return (
+            <button
+              key={element}
+              onClick={() => setFilter(element)}
+              className={` chips ${filter === element && "chip-active"}`}
+            >
+              {element}
+            </button>
+          );
+        })}
       </div>
       <div className="video-listing-container">
-        {videoList.map((item) => (
+        {filteredData.map((item) => (
           <Card
             item={item}
             isLike={likeId.includes(item._id)}
