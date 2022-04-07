1e2b87c42937295d7b2f3b8688af926a1a1dac65
Samyak Shah <samyakshah3008@gmail.com>
2022-04-07 15:22:36 +0530

    like update

--- src/components/likebox/LikeBox.jsx
+++ src/components/likebox/LikeBox.jsx
@@ -15,7 +15,6 @@ export default function LikeBox() {
     <div>
       {like.like.length === 0 ? (
         <div className="video-listing-container">
-          <div className="page-title">Liked video (0) </div>
           <div className="page-video-container">
             <div className="no-video-message">
               There Is No Liked Video. Please Add{" "}
