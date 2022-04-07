38d4718f9dcf62657de8557edfb83aa3cca5bfdd
Samyak Shah <samyakshah3008@gmail.com>
2022-04-07 15:23:19 +0530

    singlevideo component add

--- src/components/singlevideobox/SingleVideoBox.jsx
+++ src/components/singlevideobox/SingleVideoBox.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import "./singlevideobox.css"
-import * as FaIcons from "react-icons/fa";
-
+import React from "react";
+import "./singlevideobox.css";
+import { useParams } from "react-router-dom";
+import { useSingleVideo } from "../../contexts/video-listing-context";
+import "./singlevideobox.css";
 
 export default function SingleVideoBox() {
+  const { videoId } = useParams();
+  const videoObj = useSingleVideo(videoId);
+
   return (
     <div>
-        <div className="video-listing-container flex-row">
-            <img className="single-video-image" src= "https://img.youtube.com/vi/iGfT6rdTS34/hqdefault.jpg" />
-            <div className='single-video-title'>Launching Neog.Camp</div>
-            <div className="card-sub-container2">
-            <div className="card-channel-name"> Tanay Pratap </div>
-            <FaIcons.FaCheckCircle className="icons sidebar-icons" />
-          </div>
-          <div className='single-video-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus minus enim sint, accusamus aut consequuntur earum labore harum non magnam?</div>
-        </div>
+      <div className="video-listing-container flex-row">
+        <iFrame
+          className="iFrame-class"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iFrame>
+        <div className="single-video-title"> {videoObj?.cardTitle} </div>
 
+        <div className="single-video-description">
+          {" "}
+          {videoObj?.description}{" "}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
