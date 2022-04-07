adfefbf8da2c9abd6f0fb908b5711d70f93847f9
Samyak Shah <samyakshah3008@gmail.com>
2022-04-07 15:31:08 +0530

    header info update

--- src/components/hero-header/HeroHeader.jsx
+++ src/components/hero-header/HeroHeader.jsx
@@ -14,9 +14,9 @@ export default function HeroHeader() {
     <div>
         <div className="hero-container">
             
-                <h2 className='hero-header-title'>Learn New trends with</h2>
+                <h2 className='hero-header-title'>Triple chill with</h2>
                 <h1 className="logo-name">TripleS Player</h1>
-                <p className="header-description">TripleS is a learning platform for students.</p>
+                <p className="header-description">TripleS is platform for free movie watching</p>
           <Link to = "/videolisting">  <button onClick={watchNowHandler} className='watch-now-btn'>Watch now</button></Link>
                 
             
