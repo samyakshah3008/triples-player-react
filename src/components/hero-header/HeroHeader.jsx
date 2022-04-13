2503c04a88c5ddd0189bdcfa60ba22e3580f17eb
Samyak Shah <samyakshah3008@gmail.com>
2022-04-14 00:19:53 +0530

    header update

--- src/components/hero-header/HeroHeader.jsx
+++ src/components/hero-header/HeroHeader.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./heroheader.css"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../contexts/auth-context";
+import "./heroheader.css";
 
 export default function HeroHeader() {
-
-    const watchNowHandler = () => {
-        console.log("clicked")
-
-    }
+  const watchNowHandler = () => {
+    console.log("clicked");
+  };
 
   return (
     <div>
-        <div className="hero-container">
-            
-                <h2 className='hero-header-title'>Triple chill with</h2>
-                <h1 className="logo-name">TripleS Player</h1>
-                <p className="header-description">TripleS is platform for free movie watching</p>
-          <Link to = "/videolisting">  <button onClick={watchNowHandler} className='watch-now-btn'>Watch now</button></Link>
-                
-            
-        </div>
+      <div className="hero-container">
+        <h2 className="hero-header-title">Triple chill with</h2>
+        <h1 className="logo-name">TripleS Player</h1>
+        <p className="header-description">
+          TripleS is platform for free movie watching
+        </p>
+        <Link to="/videolisting">
+          {" "}
+          <button onClick={watchNowHandler} className="watch-now-btn">
+            Watch now
+          </button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
