e3120374f3eb1da5da13253d90cf889dc6a9f1e9
Samyak Shah <samyakshah3008@gmail.com>
2022-04-14 00:20:18 +0530

    playlists display on playlist page

--- src/components/playlist/Playlistbox.jsx
+++ src/components/playlist/Playlistbox.jsx
@@ -1,16 +1,67 @@
 import React from "react";
+import { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import { usePlaylist } from "../../contexts/playlist-context";
+import PlaylistCard from "../card/PlaylistCard";
+import "./playlistbox.css";
 
 export default function Playlistbox() {
+  const [createPlaylistModal, setCreatePlaylistModal] = useState(false);
+
+  const playlistHandler = () => {
+    setCreatePlaylistModal(false);
+  };
+
+  const { playlistArray } = usePlaylist();
   return (
     <div>
       <div className="video-listing-container">
-        <div className="page-title">Playlist (0) </div>
-        <div className="page-video-container">
-          <div className="no-video-message">
-            There is no playlist. Please Add{" "}
+        <div className="main-container playlist-container"></div>
+
+        <div
+          className="create-playlist-modal-wrapper"
+          style={
+            createPlaylistModal === false
+              ? { display: "none" }
+              : { display: "flex" }
+          }
+        >
+          <div className="create-playlist-modal">
+            <div className="space-between modal-close-button">
+              {" "}
+              <h4 className="text-sm">PlayList</h4>{" "}
+              <FaIcons.FaTimesCircle
+                className="icons tools-icon "
+                onClick={() => setCreatePlaylistModal(false)}
+              />
+            </div>
+            <div className="input-playlist-wrapper">
+              <input
+                type="text"
+                className="input"
+                placeholder="Enter playlist Name"
+                onChange={(event) =>
+                  setPlaylistName((prev) => ({
+                    ...prev,
+                    playlist: event.target.value,
+                  }))
+                }
+              />
+            </div>
+            <button
+              className="btn-playlist-create center"
+              onClick={() => playlistHandler()}
+            >
+              {" "}
+              <FaIcons.FaPlusCircle className="icons tools-icon icon-circle-plus" />
+              Create New Playlist
+            </button>
           </div>
-          <button className="explore-btn">Create playlist</button>
         </div>
+
+        {playlistArray.playlistData?.map((playlist) => (
+          <PlaylistCard playlistInfo={playlist} />
+        ))}
       </div>
     </div>
   );
