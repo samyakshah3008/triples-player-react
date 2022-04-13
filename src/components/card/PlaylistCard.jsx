30e13de5957ce50bddc18d1812f6c13fedfd0f99
Samyak Shah <samyakshah3008@gmail.com>
2022-04-14 00:19:38 +0530

    playlist card add

---
+++ src/components/card/PlaylistCard.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/auth-context";
+import * as FaIcons from "react-icons/fa";
+
+import axios from "axios";
+import { usePlaylist } from "../../contexts/playlist-context";
+export default function PlaylistCard({ playlistInfo }) {
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const { playlistArray, setPlaylistArray } = usePlaylist();
+
+  const deletePlaylistHandler = async () => {
+    const deletePlaylistResponse = await axios.delete(
+      `/api/user/playlists/${playlistInfo._id}`,
+
+      { headers: { authorization: user.token } }
+    );
+
+    console.log(deletePlaylistResponse.data.playlists, "delete response");
+    setPlaylistArray({
+      playlistData: [...deletePlaylistResponse.data.playlists],
+    });
+  };
+
+  const newHandler = () => {
+    navigate(`/playlist/${playlistInfo._id}`);
+  };
+
+  return (
+    <div>
+      <div className="parent">
+        <div className="card-container">
+          <div className="card-img-container">
+            <img
+              onClick={newHandler}
+              className="card-img"
+              src="https://res.cloudinary.com/dhqxln7zi/image/upload/v1648118531/new_vohp8t.jpg"
+              alt="cricket-ball"
+            />
+          </div>
+          <div
+            style={{ display: "flex", justifyContent: "space-between" }}
+            className="delete-title-container"
+          >
+            <div className="card-sub-container3"> {playlistInfo.title} </div>
+            <div>
+              <FaIcons.FaTrash
+                onClick={deletePlaylistHandler}
+                className="icons sidebar-icons"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
