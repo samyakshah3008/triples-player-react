050bf3ae085c6764744d1e8b0e19a21cda4e7409
Samyak Shah <samyakshah3008@gmail.com>
2022-04-14 13:44:26 +0530

    minor update

--- src/components/playlist/Playlistbox.jsx
+++ src/components/playlist/Playlistbox.jsx
@@ -17,49 +17,6 @@ export default function Playlistbox() {
   return (
     <div>
       <div className="video-listing-container">
-        <div className="main-container playlist-container"></div>
-
-        <div
-          className="create-playlist-modal-wrapper"
-          style={
-            createPlaylistModal === false
-              ? { display: "none" }
-              : { display: "flex" }
-          }
-        >
-          <div className="create-playlist-modal">
-            <div className="space-between modal-close-button">
-              {" "}
-              <h4 className="text-sm">PlayList</h4>{" "}
-              <FaIcons.FaTimesCircle
-                className="icons tools-icon "
-                onClick={() => setCreatePlaylistModal(false)}
-              />
-            </div>
-            <div className="input-playlist-wrapper">
-              <input
-                type="text"
-                className="input"
-                placeholder="Enter playlist Name"
-                onChange={(event) =>
-                  setPlaylistName((prev) => ({
-                    ...prev,
-                    playlist: event.target.value,
-                  }))
-                }
-              />
-            </div>
-            <button
-              className="btn-playlist-create center"
-              onClick={() => playlistHandler()}
-            >
-              {" "}
-              <FaIcons.FaPlusCircle className="icons tools-icon icon-circle-plus" />
-              Create New Playlist
-            </button>
-          </div>
-        </div>
-
         {playlistArray.playlistData?.map((playlist) => (
           <PlaylistCard playlistInfo={playlist} />
         ))}
