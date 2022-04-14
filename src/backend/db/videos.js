11b470251f2b7b8323a079530470be6696515fc6
Samyak Shah <samyakshah3008@gmail.com>
2022-04-14 13:44:04 +0530

    database update with movie description

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -14,7 +14,7 @@ export const videos = [
     cardTitle: "Dhamaal",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Dhamaal (English translation: Fun) is a 2007 Indian comedy film directed by Indra Kumar and produced by Ashok Thakeria.",
   },
 
   {
@@ -25,7 +25,7 @@ export const videos = [
     cardTitle: "Welcome",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Welcome is an Indian 2007 Hindi-language comedy film directed and co-written by Anees Bazmee with production by Firoz Nadiadwala.",
   },
 
   {
@@ -36,7 +36,7 @@ export const videos = [
     cardTitle: "Ready",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Ready is a 2011 Indian Hindi-language action comedy film directed by Anees Bazmee. Produced by T-Series Films, Rawail Grandsons Entertainment",
   },
   {
     _id: "SGwFnnS0iu4",
@@ -46,7 +46,7 @@ export const videos = [
     cardTitle: "Housefull 2",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Housefull 2 (also known as Housefull 2: The Dirty Dozen) is an 2012 Indian Hindi-language comedy film directed by Sajid Khan",
   },
   {
     _id: "yftMYuacQio",
@@ -56,7 +56,7 @@ export const videos = [
     cardTitle: "The Conjuring",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "The Conjuring is a 2013 American supernatural horror film directed by James Wan and written by Chad Hayes",
   },
   {
     _id: "eAJFVeBPXjQ",
@@ -66,7 +66,7 @@ export const videos = [
     cardTitle: "Annabelle comes home",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Annabelle Comes Home is a 2019 American supernatural horror film written and directed by Gary Dauberman, in his directorial debut,",
   },
   {
     _id: "VSaEdshUCzA",
@@ -76,7 +76,7 @@ export const videos = [
     cardTitle: "The Nun",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "The Nun is a 2018 American gothic supernatural horror film directed by Corin Hardy and written by Gary Dauberman",
   },
   {
     _id: "yrgst6eD-Dk",
@@ -86,7 +86,7 @@ export const videos = [
     cardTitle: "Conjuring summary",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "The Conjuring is a 2013 American supernatural horror film directed by James Wan and written by Chad Hayes",
   },
   {
     _id: "3gJId-oxtpc",
@@ -96,7 +96,7 @@ export const videos = [
     cardTitle: "Haunted House",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "A haunted house, spook house or ghost house in ghostlore is a house or other building often perceived as being inhabited",
   },
   {
     _id: "quNYPP97Yes",
@@ -106,7 +106,7 @@ export const videos = [
     cardTitle: "The 1920 Evil Returns",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "1920: The Evil Returns is a 2012 Indian horror film written by Vikram Bhatt and directed by Bhushan Patel.",
   },
   {
     _id: "S8ZUtWTuHT4",
@@ -116,27 +116,27 @@ export const videos = [
     cardTitle: "Spiderman No way home",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Spider-Man: No Way Home is a 2021 American superhero film based on the Marvel Comics character Spider-Man",
   },
   {
     _id: "DgfsnXSOeZk",
     videoLink: "https://www.youtube.com/watch?v=DgfsnXSOeZk",
     categoryName: "Thrill and action",
     cardImg: "https://img.youtube.com/vi/DgfsnXSOeZk/hqdefault.jpg",
-    cardTitle: "Krish 3",
+    cardTitle: "Krrish 3",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Krrish 3 is a 2013 Indian Hindi-language superhero film produced and directed by Rakesh Roshan, and written by Honey Irani and Robin Bhatt",
   },
   {
     _id: "-8p1H4A5jvQ",
     videoLink: "https://www.youtube.com/watch?v=-8p1H4A5jvQ",
     categoryName: "Thrill and action",
     cardImg: "https://img.youtube.com/vi/-8p1H4A5jvQ/hqdefault.jpg",
-    cardTitle: "Ghajni",
+    cardTitle: "Ghajini",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Ghajini is a 2008 Indian Hindi-language action thriller film written and directed by A. R. Murugadoss, jointly produced by Allu Aravind,",
   },
   {
     _id: "zBXa6Foq9ZM",
@@ -146,7 +146,7 @@ export const videos = [
     cardTitle: "Bang Bang!",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Bang Bang! is a 2014 Indian Hindi-language action comedy film directed by Siddharth Anand and written by Abbas Tyrewala, Sujoy Ghosh ",
   },
   {
     _id: "Jr3yUz0jRpg",
@@ -156,7 +156,7 @@ export const videos = [
     cardTitle: "Race 2",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Race 2 : Betrayal Is Survival (simply Race 2) is a 2013 Indian Hindi-language action crime film directed by Abbas-Mustan and written by Kiran Kotrial ",
   },
   {
     _id: "n8iqixi7lj8",
@@ -166,7 +166,7 @@ export const videos = [
     cardTitle: "Dhoom 3",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Dhoom 3 (transl. Blast 3) is a 2013 Indian Hindi-language action thriller film written and directed by Vijay Krishna Acharya",
   },
   {
     _id: "E00892hmVZM",
@@ -176,7 +176,7 @@ export const videos = [
     cardTitle: "MS Dhoni: Untold Story",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "M.S. Dhoni: The Untold Story is a 2016 Indian Hindi-language biographical sports drama film written and directed by Neeraj Pandey.",
   },
   {
     _id: "RPJP9fFnMoU",
@@ -186,7 +186,7 @@ export const videos = [
     cardTitle: "83 Full Movie",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "83 is a 2021 Indian Hindi-language sports drama film written and directed by Kabir Khan and produced by Deepika Padukone, Kabir Khan, Vishnu Vardhan Induri, Sajid Nadiadwala",
   },
   {
     _id: "xHfdt5cnrZ8",
@@ -196,7 +196,7 @@ export const videos = [
     cardTitle: "Mumbai central",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Mumbai Central: Directed by Karan Radhakrishna. With Chetanya Adib, Jhuma Biswas, Alibail Blackburn, Rajshri Deshpande. ",
   },
   {
     _id: "fO1mbR7zyF8",
@@ -206,7 +206,7 @@ export const videos = [
     cardTitle: "Sanju",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Sanju is a 2018 Indian Hindi-language biogrophical comedy drama film directed by Rajkumar Hirani and written by Hirani and Abhijat Joshi",
   },
   {
     _id: "WvS9jmc2-kw",
@@ -216,7 +216,7 @@ export const videos = [
     cardTitle: "The Kashmir files",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "The Kashmir Files is a 2022 Indian Hindi-language drama film written and directed by Vivek Agnihotri. The film presents a fictional storyline centred",
   },
   {
     _id: "LGB61HyL-IQ",
@@ -226,6 +226,6 @@ export const videos = [
     cardTitle: "Chakde India",
     cardOwner: "Samyak Shah",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ut voluptatem doloribus et? Mollitia, dolores minus quidem suscipit distinctio et!",
+      "Chak De! India (transl. Let's go! India) is a 2007 Indian Hindi-language sports film directed by Shimit Amin and produced by Aditya Chopra",
   },
 ];
